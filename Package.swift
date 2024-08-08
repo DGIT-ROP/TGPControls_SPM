254ba04360a57e113a07dc413222c42b135636cc
DGIT-ROP <dgit.rop@gmail.com>
2024-08-08 15:28:18 +0400

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "TGPControls",
+    products: [
+        .library(
+            name: "TGPControls",
+            targets: ["TGPControls"]),
+    ],
+    targets: [
+        .target(
+            name: "TGPControls",
+            dependencies: [],
+            path: "TGPControls",
+            exclude: ["Info.plist", "TGPControls.h"])
+    ]
+)
